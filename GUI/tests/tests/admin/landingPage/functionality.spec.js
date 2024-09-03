858755fc2785ffab425c23f5652a5249cff23379
Jakob Tulve <jakobt@jakobt-5310.ria.ee>
2024-09-03 15:15:02 +0300

    added tests

--- GUI/tests/tests/admin/landingPage/functionality.spec.js
+++ GUI/tests/tests/admin/landingPage/functionality.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const adminPage = 'http://localhost:3001'
+const adminPage = 'https://admin.prod.buerokratt.ee/'
 let response;
 
 test.beforeEach(async ({ page }) => {
