6156be9c8821ffe854a04947bed0fa5d8e4f8a11
jaX10bt <132996313+jaX10bt@users.noreply.github.com>
2024-09-03 16:42:31 +0300

    Update functionality.spec.js

--- GUI/tests/tests/admin/landingPage/functionality.spec.js
+++ GUI/tests/tests/admin/landingPage/functionality.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const adminPage = 'https://admin.prod.buerokratt.ee/'
+const adminPage = 'http://localhost:3001'
 let response;
 
 test.beforeEach(async ({ page }) => {
